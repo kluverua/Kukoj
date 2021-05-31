082df7a56c9d2593e7541a5462da4303f0157390
kluverua <kluverua@gmail.com>
2021-05-31 09:30:26 +0300

    test

--- kukoj/lib/main.dart
+++ kukoj/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 void main() {
   runApp(MyApp());
@@ -48,15 +49,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
+  Future<void> _incrementCounter() async {
+    /*setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
-    });
+    });*/
+    List<AssetPathEntity> albums = await PhotoManager.getAssetPathList();
+    print(albums);
   }
 
   @override
